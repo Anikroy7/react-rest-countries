583c8c723ad4d741a6cb50d808405047673c30b9
Anikroy7 <roy645800@gmail.com>
2022-03-18 13:50:10 +0600

    display all countries

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
+import { isCompositeComponent } from 'react-dom/test-utils';
+const countriesStyle = {
+  backgroundColor: 'lightBlue',
+  margin: '20px',
+  padding: '20px',
+  border: '4px solid red',
+  borderRadius: '20px'
 
+}
 function App() {
   return (
     <div className="App">
@@ -27,9 +35,25 @@ const LoadCountries = () => {
     <div>
       <h1>Here We Get All Countries In The World</h1>
       <h3>{countries.length}</h3>
+      {
+        countries.map(country => <Country
+          name={country.name} population={country.population}
+          capital={country.capital}
+        ></Country>)
+      }
     </div>
   )
 }
 
+const
+  Country = (props) => {
+    return (
+      <div style={countriesStyle}>
+        <h1>name: {props.name.common}</h1>
+        <h3>population: {props.population}</h3>
+        <h4>capital: {props.capital}</h4>
+      </div>
+    )
+  }
 
 export default App;
